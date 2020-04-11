28cebb9b379d6306fdfb73a51979dcfefa14ff52
artemship <avs.artem@list.ru>
2020-04-11 08:54:08 +0300

    add calculation form

--- templates/calculation.php
+++ templates/calculation.php
@@ -1,167 +1,182 @@
-<section id="form-parameter">
+<h1>Калькулятор КАСКО</h1>
 
+<div class="calculation-container">
     <form action="policy" method="post">
-        <div class="container">
 
-            <!--            <div class="col-5 form-group">-->
-            <!--                <label for="exampleFormControlSelect1">Марка</label>-->
-            <!--                <input class="form-control" type="text" id="js-mark" placeholder="Введите марку">-->
-            <!--                <div id="js-mark-list"></div>-->
-            <!--            </div>-->
 
-            <div class="row">
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Марка</label>
-                    <select class="form-control" id="js-select-mark" onchange="nulltariff()">
-                        <option value="0">--Выбрать марку--</option>
-                        <?php if (!empty($marks)):
-                            foreach ($marks as $mark): ?>
-                                <option value="<?= $mark; ?>"><?= $mark ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Франшиза</label>
-                    <select class="form-control" id="js-select-franchise" onchange="nulltariff()">
-                        <?php if (!empty($franchises)):
-                            foreach ($franchises as $franchise): ?>
-                                <option value="<?= $franchise; ?>"><?= $franchise . ' %' ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
+        <div class="table calculation">
+
+            <div class="table-row">
+                <label class="calculation-label">Марка</label>
+                <select class="calculation-content" id="js-select-mark" onchange="nulltariff()">
+                    <option value="0">--Выбрать марку--</option>
+                    <?php if (!empty($marks)):
+                        foreach ($marks as $mark): ?>
+                            <option value="<?= $mark; ?>"><?= $mark ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
 
-            <div class="row">
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Модель</label>
-                    <select class="form-control" id="js-select-model" onchange="nulltariff()" disabled>
-                        <option value="0">--Выбрать модель--</option>
-                    </select>
-                    <!--                <span class="model"></span>-->
-                </div>
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Порядок оплаты страховой премии</label>
-                    <select class="form-control " id="js-select-payment-procedure">
-                        <?php if (!empty($paymentProcedures)):
-                            foreach ($paymentProcedures as $key => $option): ?>
-                                <option value="<?= $key; ?>"><?= $option ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
+            <div class="table-row">
+                <label class="calculation-label">Модель</label>
+                <select class="calculation-content" id="js-select-model" onchange="nulltariff()" disabled>
+                    <option value="0">--Выбрать модель--</option>
+                </select>
             </div>
 
-            <div class="row">
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Год выпуска</label>
-                    <select class="form-control " id="js-select-car-age" onchange="nulltariff()">
-                        <?php if (!empty($carsAge)):
-                            foreach ($carsAge as $key => $carAge): ?>
-                                <option value="<?= $key; ?>"><?= $carAge ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1"><b>Страховая сумма</b></label>
-                    <input class="form-control" type="text" id="js-insurance-sum" placeholder="Страховая сумма"
-                           value="1100000">
-                </div>
+            <div class="table-row">
+                <label class="calculation-label">Год выпуска</label>
+                <select class="calculation-content" id="js-select-car-age" onchange="nulltariff()">
+                    <?php if (!empty($carsAge)):
+                        foreach ($carsAge as $key => $carAge): ?>
+                            <option value="<?= $key; ?>"><?= $carAge ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
 
-            <div class="row">
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Страховой риск</label>
-                    <select class="form-control " id="js-select-insurance" onchange="nulltariff()">
-                        <?php if (!empty($insurances)):
-                            foreach ($insurances as $key => $insurance): ?>
-                                <option value="<?= $key; ?>"><?= $insurance ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Возраст водителя</label>
-                    <input class="form-control" type="text" id="js-age" placeholder="Возраст водителя"
-                           onchange="nulltariff()">
-                    <p class="alert-danger" id="error_v"></p>
-                </div>
+            <div class="table-row">
+                <label class="calculation-label">Страховой риск</label>
+                <select class="calculation-content" id="js-select-insurance" onchange="nulltariff()">
+                    <?php if (!empty($insurances)):
+                        foreach ($insurances as $key => $insurance): ?>
+                            <option value="<?= $key; ?>"><?= $insurance ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
 
-            <div class="row">
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Срок страхования</label>
-                    <select class="form-control " id="js-select-period">
-                        <?php if (!empty($periods)):
-                            foreach ($periods as $key => $period):
-                                if ($key === 6): ?>
-                                    <option value="<?= $key; ?>" selected><?= $period ?></option>
-                                    <? continue;
-                                endif; ?>
-                                <option value="<?= $key; ?>"><?= $period ?></option>
-                            <? endforeach;
-                        endif ?>
-                    </select>
-                </div>
-                <div class="col-6 form-group label-font">
-                    <label for="exampleFormControlSelect1">Стаж</label>
-                    <input class="form-control" type="text" id="js-experience" placeholder="Стаж"
-                           onchange="nulltariff()">
-                    <p class="alert-danger" id="error_s"></p>
-                </div>
+            <div class="table-row">
+                <label class="calculation-label">Франшиза</label>
+                <select class="calculation-content" id="js-select-franchise" onchange="nulltariff()">
+                    <?php if (!empty($franchises)):
+                        foreach ($franchises as $franchise): ?>
+                            <option value="<?= $franchise; ?>"><?= $franchise . ' %' ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-9 custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" value="" id="customCheck1" checked>
-                        <label class="custom-control-label" for="customCheck1">
-                            ТС находится на гарантии производителя
-                        </label>
-                    </div>
-
-                    <div class="col-9 custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" value="" id="customCheck2" checked>
-                        <label class="custom-control-label" for="customCheck2">
-                            Отсутствует выплата без справок из компетентных органов (один стеклянный элемент)
-                        </label>
-                    </div>
-
-                    <div class="col-9 custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" value="" id="customCheck3" checked>
-                        <label class="custom-control-label" for="customCheck3">
-                            Отсутствует выплата без справок из компетентных органов (один кузовной элемент)
-                        </label>
-                    </div>
-
-                    <div class="col-9 custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" value="" id="customCheck4" checked>
-                        <label class="custom-control-label" for="customCheck4">
-                            Агрегатная
-                        </label>
-                    </div>
-                </div>
+            <div class="table-row">
+                <label class="calculation-label">Порядок оплаты страховой премии</label>
+                <select class="calculation-content" id="js-select-payment-procedure">
+                    <?php if (!empty($paymentProcedures)):
+                        foreach ($paymentProcedures as $key => $option): ?>
+                            <option value="<?= $key; ?>"><?= $option ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
 
-            <div class="container">
-                <div class="col-6 form-group">
-                    <label for="exampleFormControlSelect1"><b>Тариф</b></label>
-                    <input class="form-control" type="text" id="tariff" disabled>
-                </div>
-
-                <div class=" col-12 ">
-                    <button type="button" class="btn btn-secondary" id="js-btn-submit">Рассчитать</button>
-                    <button type="button" class="btn btn-secondary" id="js-btn-print">Печать</button>
-                </div>
-
-
+            <div class="table-row">
+                <label class="calculation-label">Срок страхования</label>
+                <select class="calculation-content" id="js-select-period">
+                    <?php if (!empty($periods)):
+                        foreach ($periods as $key => $period):
+                            if ($key === 6): ?>
+                                <option value="<?= $key; ?>" selected><?= $period ?></option>
+                                <? continue;
+                            endif; ?>
+                            <option value="<?= $key; ?>"><?= $period ?></option>
+                        <? endforeach;
+                    endif ?>
+                </select>
             </div>
+
+            <div class="table-row">
+                <label class="calculation-label">Страховая сумма</label>
+                <input class="calculation-content" type="text" id="js-insurance-sum" value="1100000">
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label">Возраст водителя</label>
+                <input class="calculation-content" type="text" id="js-age" value="22" onchange="nulltariff()">
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label">Стаж</label>
+                <input class="calculation-content" type="text" id="js-experience" value="1" onchange="nulltariff()">
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label"></label>
+                <label class="b-contain">
+                    <span>Агрегатная</span>
+                    <input type="checkbox" value="" id="customCheck1" checked>
+                    <div class="b-input"></div>
+                </label>
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label"></label>
+                <label class="b-contain">
+                    <span>ТС находится на гарантии производителя</span>
+                    <input type="checkbox" value="" id="customCheck2" checked>
+                    <div class="b-input"></div>
+                </label>
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label"></label>
+                <label class="b-contain">
+                    <span>Отсутствует выплата без справок из компетентных органов (один стеклянный элемент)</span>
+                    <input type="checkbox" value="" id="customCheck3" checked>
+                    <div class="b-input"></div>
+                </label>
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label"></label>
+                <label class="b-contain">
+                    <span>Отсутствует выплата без справок из компетентных органов (один кузовной элемент)</span>
+                    <input type="checkbox" value="" id="customCheck4" checked>
+                    <div class="b-input"></div>
+                </label>
+            </div>
+
+            <div class="table-row">
+                <label class="calculation-label">Тариф</label>
+                <input class="calculation-content" type="text" id="tariff" disabled>
+            </div>
+
+            <div class="table-row ta-right">
+                <label class="calculation-label"></label>
+<!--                <button type="button" class="btn btn-secondary" id="js-btn-submit">Рассчитать</button>-->
+<!--                <input class="calculation-button" type="submit" value="Назад">-->
+<!--                <input class="calculation-button" type="submit" value="Далее">-->
+                <button type="button" class="calculation-button btn btn-secondary" id="js-btn-submit">Рассчитать</button>
+                <button type="button" class="calculation-button btn btn-secondary" id="js-btn-print">Печать</button>
+            </div>
+
         </div>
+
+        <div  class="flex-row center">
+
+        </div>
+
+
+
+<!--        <div class="form">-->
+<!--            <div class="form__row">-->
+<!--                <div class="form__label">-->
+<!--                    <span>ПТС</span>-->
+<!--                </div>-->
+<!--                <div class="form__content">-->
+<!---->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="form__row">-->
+<!--                <div class="form__label">-->
+<!--                    <span>VIN number</span>-->
+<!--                </div>-->
+<!--                <div class="form__content">-->
+<!---->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+
+
+
     </form>
-</section>
-
-
-
-
+</div>
--- www/js/selectMark.js
+++ www/js/selectMark.js
@@ -136,37 +136,39 @@ $(function () {
             $("#tariff").val('Выберите марку');
             return;
         }
-        if (age < 18) {
-            //$("#js-age").val('Возраст должен быть больше 18 лет!');
-            document.getElementById("error_v").innerHTML = "Возраст должен быть больше 18 лет!";
-            document.getElementById("js-age").style.borderColor = "#AA0000";
-            document.getElementById("error_v").style.fontFamily = "cursive";
-            return;
-        } else {
-            document.getElementById("error_v").innerHTML = null;
-            document.getElementById("js-age").style.borderColor = null;
-        }
-        if (((age - experience) < 18) || (experience == '')) {
-            document.getElementById("error_s").innerHTML = "Стаж указан неверно, проверьте введенные данные!";
-            document.getElementById("js-experience").style.borderColor = "#AA0000";
-            document.getElementById("error_s").style.fontFamily = "cursive";
-            return;
-        } else {
-            document.getElementById("error_s").innerHTML = null;
-            document.getElementById("js-experience").style.borderColor = null;
-            //$("#js-experience").val('Стаж указан неверно, проверьте введенные данные!');
-        }
+
+        // if (age < 18) {
+        //     ///$("#js-age").val('Возраст должен быть больше 18 лет!');
+        //     document.getElementById("error_v").innerHTML = "Возраст должен быть больше 18 лет!";
+        //     document.getElementById("js-age").style.borderColor = "#AA0000";
+        //     document.getElementById("error_v").style.fontFamily = "cursive";
+        //     return;
+        // } else {
+        //     document.getElementById("error_v").innerHTML = null;
+        //     document.getElementById("js-age").style.borderColor = null;
+        // }
+        // if (((age - experience) < 18) || (experience == '')) {
+        //     document.getElementById("error_s").innerHTML = "Стаж указан неверно, проверьте введенные данные!";
+        //     document.getElementById("js-experience").style.borderColor = "#AA0000";
+        //     document.getElementById("error_s").style.fontFamily = "cursive";
+        //     return;
+        // } else {
+        //     document.getElementById("error_s").innerHTML = null;
+        //     document.getElementById("js-experience").style.borderColor = null;
+        //     ///$("#js-experience").val('Стаж указан неверно, проверьте введенные данные!');
+        // }
+
         if (customCheck1.checked) {
-            isWarranty = 1;
+            isAggregate = 1;
         }
         if (customCheck2.checked) {
-            noGlassPayment = 1;
+            isWarranty = 1;
         }
         if (customCheck3.checked) {
-            noBodyPayment = 1;
+            noGlassPayment = 1;
         }
         if (customCheck4.checked) {
-            isAggregate = 1;
+            noBodyPayment = 1;
         }
 
         if (target.id === "js-btn-submit") {
