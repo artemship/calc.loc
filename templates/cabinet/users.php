c009eaa9b471acd063bcb49dd5e8e972d4ecd12e
artemship <avs.artem@list.ru>
2020-04-24 14:26:02 +0300

    finish users

--- templates/cabinet/users.php
+++ templates/cabinet/users.php
@@ -1,7 +1,6 @@
 <script src="/js/cabinet/users.js"></script>
 <form action="/cabinet/users" method="post">
 
-
     <div class="flex-row" style="justify-content: space-between; margin-bottom: 15px">
         <div>
 <!--            <select name="usersPerPage" onchange="this.form.submit()">-->
@@ -10,40 +9,23 @@
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
-                <option value="10">10</option>
+                <option value="10" selected>10</option>
                 <option value="15">15</option>
                 <option value="25">25</option>
                 <option value="50">50</option>
                 <option value="100">100</option>
             </select>
-            <label>На странице</label>
+            <label id="js-test-sort">На странице</label>
         </div>
-        <input type="text" placeholder="Поиск">
+        <input id="js-search-users" type="text" placeholder="Поиск">
     </div>
 
     <div class="cabinet_users_table" id="js-users-table">
-<!--        JS Insert table-->
+        <!--JS Insert table-->
     </div>
 
     <div class="navigation" id="js-navigation">
-<!--        <label value="1" class="edge">1</label>-->
-<!--        <label value="3">2</label>-->
-<!--        <label value="1">3</label>-->
-<!--        <label value="3">4</label>-->
-<!--        <label value="1">5</label>-->
-<!--        <label value="1">6</label>-->
-<!--        <label value="3" class="selected">7</label>-->
-<!--        <label value="1">8</label>-->
-<!--        <label value="3">9</label>-->
-<!--        <label value="1">10</label>-->
-<!--        <label value="1">11</label>-->
-<!--        <label value="1">12</label>-->
-<!--        <label value="1">13</label>-->
-<!--        <label value="1" class="edge">14</label>-->
-
-<!--        <a href="?page=1">1</a>-->
-<!--        <a href="?page=2" class="selected">2</a>-->
-<!--        <a href="?page=3">3</a>-->
+        <!--JS Insert page navigation-->
     </div>
 
 
--- www/js/cabinet/users.js
+++ www/js/cabinet/users.js
@@ -11,22 +11,58 @@ $(function () {
     };
     xhr.send(null);
 
-    $("#js-amount-per-page").change(function () {
+    let sortOptions = [];
+
+    const searchEdit = document.getElementById("js-search-users");
+    searchEdit.addEventListener('input', () => filterUsers(1));
+
+    function filterUsers(pageNumber) {
         $('#js-users-table').empty();
         $('#js-navigation').empty();
-        outputHtml(users, 1);
+        let filterUsers = searchUsers(searchEdit.value);
+        outputHtml(filterUsers, pageNumber, sortOptions);
+    }
+
+    function searchUsers(searchText) {
+        let matches = users.filter(user => {
+            const regex = new RegExp(`${searchText}`, 'gi');
+            return user.login.match(regex)
+                + user.first_name.match(regex)
+                + user.last_name.match(regex)
+                + user.email.match(regex)
+                + user.role.match(regex);
+        });
+        console.log(matches);
+        return matches;
+    }
+
+    function byField(field) {
+        return sortOptions[0] === 'asc'
+            ? (a, b) => a[field] > b[field] ? 1 : -1
+            : (a, b) => a[field] > b[field] ? -1 : 1
+    }
+
+    function switchOrder() {
+        return sortOptions[0] === 'asc' ? sortOptions[0] = 'desc' : sortOptions[0] = 'asc'
+    }
+
+    function sort(event) {
+        let value = event.target.value;
+        !!sortOptions && (sortOptions[1] === value) ? switchOrder() : sortOptions = ['asc', value];
+        users.sort(byField(value));
+        filterUsers(1, sortOptions);
+    }
+
+    $("#js-amount-per-page").change(function () {
+        filterUsers(1, sortOptions);
     });
 
     function selectPage(event) {
-        let pageNumber = event.target.value
-        $('#js-users-table').empty();
-        $('#js-navigation').empty();
-        outputHtml(users, pageNumber);
+        let pageNumber = event.target.value;
+        filterUsers(pageNumber, sortOptions);
     }
 
-    function outputHtml(users, pageNumber) {
-        // alert(u[0]['login']);
-        // const outputHtml = users => {
+    function outputHtml(users, pageNumber, sortOptions) {
         pageNumber = Number(pageNumber);
         let amountPerPage = Number(document.getElementById("js-amount-per-page").value);
 
@@ -35,15 +71,26 @@ $(function () {
 
         let userLogin = document.createElement("div");
         userLogin.className = 'cabinet_users_table-cell';
+        userLogin.value = 'login';
+        userLogin.addEventListener('click', sort);
         let userName = document.createElement("div");
         userName.className = 'cabinet_users_table-cell';
+        userName.value = 'last_name';
+        userName.addEventListener('click', sort);
         let userEmail = document.createElement("div");
         userEmail.className = 'cabinet_users_table-cell';
+        userEmail.value = 'email';
+        userEmail.addEventListener('click', sort);
         let userRole = document.createElement("div");
         userRole.className = 'cabinet_users_table-cell';
+        userRole.value = 'role';
+        userRole.addEventListener('click', sort);
         let userAccess = document.createElement("div");
-        userAccess.className = 'cabinet_users_table-cell';
-
+        userAccess.className = 'cabinet_users_table-cell unsorted';
+        if (!!sortOptions && sortOptions[1] === 'login') userLogin.className += ' ' + sortOptions[0];
+        if (!!sortOptions && sortOptions[1] === 'last_name') userName.className += ' ' + sortOptions[0];
+        if (!!sortOptions && sortOptions[1] === 'email') userEmail.className += ' ' + sortOptions[0];
+        if (!!sortOptions && sortOptions[1] === 'role') userRole.className += ' ' + sortOptions[0];
         userLogin.innerHTML = 'Логин';
         userName.innerHTML = 'Имя';
         userEmail.innerHTML = 'Email';
@@ -56,135 +103,130 @@ $(function () {
         row.appendChild(userAccess);
         document.getElementById('js-users-table').appendChild(row);
 
+        if (users.length < 1) {
+            return;
+        }
+        let startingId = (pageNumber - 1) * amountPerPage;
+        let i = startingId;
+        while (i < Math.min(users.length, startingId + amountPerPage)) {
+            row = document.createElement("div");
+            row.className = 'cabinet_users_table-row';
+            userLogin = document.createElement("div");
+            userLogin.className = 'cabinet_users_table-cell';
+            userName = document.createElement("div");
+            userName.className = 'cabinet_users_table-cell';
+            userEmail = document.createElement("div");
+            userEmail.className = 'cabinet_users_table-cell';
+            userRole = document.createElement("div");
+            userRole.className = 'cabinet_users_table-cell';
+            userAccess = document.createElement("div");
+            userAccess.className = 'cabinet_users_table-cell';
 
-        if (users.length > 0) {
-            let startingId = (pageNumber - 1) * amountPerPage;
-            let i = startingId;
-            while (i < Math.min(users.length, startingId + amountPerPage)) {
-                row = document.createElement("div");
-                row.className = 'cabinet_users_table-row';
+            const labelSwitch = document.createElement("label");
+            labelSwitch.className = 'switch';
 
-                userLogin = document.createElement("div");
-                userLogin.className = 'cabinet_users_table-cell';
+            const inputCheckBox = document.createElement("input");
+            inputCheckBox.className = 'js-provide-access';
+            inputCheckBox.type = 'checkbox';
+            inputCheckBox.name = users[i]['id'];
+            if (users[i]['is_accessed'] === '1') {
+                inputCheckBox.setAttribute('checked', '');
+            }
+            inputCheckBox.addEventListener('click', switchAccess);
 
-                userName = document.createElement("div");
-                userName.className = 'cabinet_users_table-cell';
+            const span = document.createElement("span");
+            span.className = 'slider round';
 
-                userEmail = document.createElement("div");
-                userEmail.className = 'cabinet_users_table-cell';
+            labelSwitch.appendChild(inputCheckBox);
+            labelSwitch.appendChild(span);
+            userAccess.appendChild(labelSwitch);
 
-                userRole = document.createElement("div");
-                userRole.className = 'cabinet_users_table-cell';
+            userLogin.innerHTML = users[i]['login'];
+            userName.innerHTML = users[i]['last_name'] + ' ' + users[i]['first_name'];
+            userEmail.innerHTML = users[i]['email'];
+            userRole.innerHTML = users[i]['role'];
 
-                userAccess = document.createElement("div");
-                userAccess.className = 'cabinet_users_table-cell';
+            row.appendChild(userLogin);
+            row.appendChild(userName);
+            row.appendChild(userEmail);
+            row.appendChild(userRole);
+            row.appendChild(userAccess);
 
-                const labelSwitch = document.createElement("label");
-                labelSwitch.className = 'switch';
+            document.getElementById('js-users-table').appendChild(row);
+            i++;
+        }
 
-                const inputCheckBox = document.createElement("input");
-                inputCheckBox.className = 'js-provide-access';
-                inputCheckBox.type = 'checkbox';
-                inputCheckBox.name = users[i]['id'];
-                if (users[i]['is_accessed'] === '1') {
-                    inputCheckBox.setAttribute('checked', '');
+        let numberOfPages = Math.ceil(users.length / amountPerPage);
+        if (numberOfPages <= 1) {
+            return;
+        }
+        let toBegin = false;
+        let toEnd = false;
+        if (pageNumber > 4) {
+            toBegin = true;
+        }
+        if (numberOfPages - pageNumber > 3) {
+            toEnd = true;
+        }
+
+        for (let i = 1; i <= numberOfPages; i++) {
+            if ((i === 1 || i === numberOfPages) && numberOfPages >= 7) {
+                let page = document.createElement('label');
+                if (i === pageNumber) {
+                    page.className = 'selected';
                 }
-                inputCheckBox.addEventListener('click', switchAccess);
-
-                const span = document.createElement("span");
-                span.className = 'slider round';
-
-                labelSwitch.appendChild(inputCheckBox);
-                labelSwitch.appendChild(span);
-                userAccess.appendChild(labelSwitch);
-
-                userLogin.innerHTML = users[i]['login'];
-                userName.innerHTML = users[i]['last_name'] + ' ' + users[i]['first_name'];
-                userEmail.innerHTML = users[i]['email'];
-                userRole.innerHTML = users[i]['role'];
-
-                row.appendChild(userLogin);
-                row.appendChild(userName);
-                row.appendChild(userEmail);
-                row.appendChild(userRole);
-                row.appendChild(userAccess);
-
-                document.getElementById('js-users-table').appendChild(row);
-                i++;
+                if (toBegin === true && i === 1 && numberOfPages !== 7) {
+                    page.className = ' edge';
+                }
+                if (toEnd === true && i === numberOfPages && numberOfPages !== 7) {
+                    page.className = ' edge';
+                }
+                page.innerHTML = String(i);
+                page.value = String(i);
+                page.addEventListener('click', selectPage);
+                document.getElementById('js-navigation').appendChild(page);
+                continue;
             }
 
+            let leftEdge;
+            let rightEdge;
 
-            let numberOfPages = Math.ceil(users.length / amountPerPage);
-            if (numberOfPages > 1) {
-                let toBegin = false;
-                let toEnd = false;
-                if (pageNumber > 4) {
-                    toBegin = true;
-                }
-                if (numberOfPages - pageNumber > 3) {
-                    toEnd = true;
-                }
-
-                for (let i = 1; i <= numberOfPages; i++) {
-                    if ((i === 1 || i === numberOfPages) && numberOfPages >= 7) {
-                        let page = document.createElement('label');
-                        if (i === pageNumber) {
-                            page.className = 'selected';
-                        }
-                        if (toBegin === true && i === 1) {
-                            page.className = ' edge';
-                        }
-                        if (toEnd === true && i === numberOfPages) {
-                            page.className = ' edge';
-                        }
-                        page.innerHTML = String(i);
-                        page.value = String(i);
-                        page.addEventListener('click', selectPage);
-                        document.getElementById('js-navigation').appendChild(page);
-                        continue;
-                    }
-
-                    let leftEdge;
-                    let rightEdge;
-
-                    switch (pageNumber) {
-                        case 1 :
-                            rightEdge = 5;
-                            break;
-                        case 2 :
-                            rightEdge = 4;
-                            break;
-                        case 3 :
-                            rightEdge = 3;
-                            break;
-                        case numberOfPages - 2:
-                            leftEdge = 3;
-                            break;
-                        case numberOfPages - 1:
-                            leftEdge = 4;
-                            break;
-                        case numberOfPages:
-                            leftEdge = 5;
-                            break;
-                        default:
-                            rightEdge = 2;
-                            leftEdge = 2;
-                    }
-
-                    if (i < pageNumber - leftEdge || i > pageNumber + rightEdge) {
-                        continue;
-                    }
-
-                    let page = document.createElement('label');
-                    if (i === pageNumber) {
-                        page.className = 'selected';
-                    }
-                    page.value = String(i);
-                    page.innerHTML = String(i);
-                    page.addEventListener('click', selectPage);
-                    document.getElementById('js-navigation').appendChild(page);
-                }
+            switch (pageNumber) {
+                case 1 :
+                    rightEdge = 5;
+                    break;
+                case 2 :
+                    rightEdge = 4;
+                    break;
+                case 3 :
+                    rightEdge = 3;
+                    break;
+                case numberOfPages - 2:
+                    leftEdge = 3;
+                    break;
+                case numberOfPages - 1:
+                    leftEdge = 4;
+                    break;
+                case numberOfPages:
+                    leftEdge = 5;
+                    break;
+                default:
+                    rightEdge = 2;
+                    leftEdge = 2;
             }
+
+            if (i < pageNumber - leftEdge || i > pageNumber + rightEdge) {
+                continue;
+            }
+
+            let page = document.createElement('label');
+            if (i === pageNumber) {
+                page.className = 'selected';
+            }
+            page.value = String(i);
+            page.innerHTML = String(i);
+            page.addEventListener('click', selectPage);
+            document.getElementById('js-navigation').appendChild(page);
         }
     }
 
